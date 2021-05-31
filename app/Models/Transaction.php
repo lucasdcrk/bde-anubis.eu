567d97e083822e26d1cf781c8376e53922c53156
Lucas Decrock <lucas@decrock.me>
2021-05-31 11:18:46 +0200

    Add transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($query) {
+            $query->admin_id = $query->admin_id ?? auth()->user()->id;
+        });
+    }
 }
